4691e8c33084821574b41863bfb8f6973b7c94f2
maehw <maehw@posteo.de>
2021-11-14 17:16:06 +0100

    Add GUI prototype script to list of code to be checked

--- check_code.py
+++ check_code.py
@@ -8,7 +8,7 @@ def separator():
 
 if __name__ == '__main__':
     # perform checks with pycodestyle
-    files = ['check_code.py', 'ms_teams_hand_detector.py']
+    files = ['check_code.py', 'ms_teams_hand_detector.py', 'gui.py']
     for file in files:
         fchecker = pycodestyle.Checker(file, show_source=True)
         num_errors = fchecker.check_all(expected=["E501"])  # allow long lines
